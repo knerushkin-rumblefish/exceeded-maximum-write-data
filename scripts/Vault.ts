c2cfbb83f0ff23c188cb418db39a6f4ec6f2af18
Kanstantsin Nerushkin <kanstantsin.nerushkin@rumblefish.pl>
2022-05-09 12:10:37 +0200

    manual contract gas limit

--- scripts/Vault.ts
+++ scripts/Vault.ts
@@ -12,11 +12,11 @@ export const MONTH = DAY * 30
 export async function deployAuthorizer() {
   try {
     const AuthorizerFactory = await ethers.getContractFactory("Authorizer")
-    const authorizer = await AuthorizerFactory.deploy("0xbB91644F26b075bda47a13682DAD006eb9d70867", { gasLimit: 12000000 })
+    const authorizer = await AuthorizerFactory.deploy("0xbB91644F26b075bda47a13682DAD006eb9d70867", { gasLimit: 4500000 })
     console.log('Authorizer', authorizer.address)
 
     const VaultFactory = await ethers.getContractFactory("Vault")
-    const vault = await VaultFactory.deploy(authorizer, constants.AddressZero, 3 * MONTH, MONTH, { gasLimit: 12000000 })
+    const vault = await VaultFactory.deploy(authorizer.address, constants.AddressZero, 3 * MONTH, MONTH, { gasLimit: 4500000 })
     console.log('Vault', vault.address)
   } catch(error) {
     console.error(error)
